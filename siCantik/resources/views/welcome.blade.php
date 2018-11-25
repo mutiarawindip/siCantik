977ae633c4a53020d8eaf0eed11aadb46b6a81c5
Mutiara <mutiarawindi@gmail.com>
2018-11-25 21:11:34 +0700

    etc

--- siCantik/resources/views/welcome.blade.php
+++ siCantik/resources/views/welcome.blade.php
@@ -27,39 +27,39 @@
         <div class="row">
             <div class="col-md-4 ftco-animate">
                 <a href="#" class="work-entry">
-                    <img src="pretty-gh-pages/images/work-1.jpg" class="img-fluid" alt="Colorlib Template">
+                    <img src="pretty-gh-pages/images/image_2.jpg" class="img-fluid" alt="Face Care">
                     <div class="info d-flex align-items-center">
                         <div>
                             <div class="icon mb-4 d-flex align-items-center justify-content-center">
                                 <span class="icon-search"></span>
                             </div>
-                            <h3>Lorem ipsum</h3>
+                            <h3>Face Care</h3>
                         </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 ftco-animate">
                 <a href="#" class="work-entry">
-                    <img src="pretty-gh-pages/images/work-2.jpg" class="img-fluid" alt="Colorlib Template">
+                    <img src="pretty-gh-pages/images/header.jpg" class="img-fluid" alt="Acne Solution">
                     <div class="info d-flex align-items-center">
                         <div>
                             <div class="icon mb-4 d-flex align-items-center justify-content-center">
                                 <span class="icon-search"></span>
                             </div>
-                            <h3>Lorem ipsum</h3>
+                            <h3>Acne Solution</h3>
                         </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 ftco-animate">
                 <a href="#" class="work-entry">
-                    <img src="pretty-gh-pages/images/work-3.jpg" class="img-fluid" alt="Colorlib Template">
+                    <img src="pretty-gh-pages/images/1.jpg" class="img-fluid" alt="Fair Skin">
                     <div class="info d-flex align-items-center">
                         <div>
                             <div class="icon mb-4 d-flex align-items-center justify-content-center">
                                 <span class="icon-search"></span>
                             </div>
-                            <h3>Lorem ipsum</h3>
+                            <h3>Fair Skin</h3>
                         </div>
                     </div>
                 </a>
@@ -68,7 +68,7 @@
     </div>
 </section>
 
-<section class="ftco-partner bg-light">
+<section class="ftco-partner bg-light" style="margin-bottom: 50px">
     <div class="container">
         <div class="row partner justify-content-center">
             <div class="col-md-10">
@@ -97,64 +97,6 @@
             </div>
         </div>
     </div>
-</section>
-
-<section class="ftco-section">
-  <div class="container">
-    <div class="row justify-content-center mb-5 pb-3">
-      <div class="col-md-7 heading-section ftco-animate text-center">
-        <h2 class="mb-4">Recent from blog</h2>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-    </div>
-</div>
-<div class="row d-flex">
-  <div class="col-md-4 d-flex ftco-animate">
-    <div class="blog-entry align-self-stretch">
-      <a href="blog-single.html" class="block-20" style="background-image: url('pretty-gh-pages/images/image_1.jpg');">
-      </a>
-      <div class="text py-4 d-block">
-        <div class="meta">
-          <div><a href="#">Sept 10, 2018</a></div>
-          <div><a href="#">Admin</a></div>
-          <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-      </div>
-      <h3 class="heading mt-2"><a href="#">Skin Care for Teen Skin</a></h3>
-      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-  </div>
-</div>
-</div>
-<div class="col-md-4 d-flex ftco-animate">
-    <div class="blog-entry align-self-stretch">
-      <a href="blog-single.html" class="block-20" style="background-image: url('pretty-gh-pages/images/image_2.jpg');">
-      </a>
-      <div class="text py-4 d-block">
-        <div class="meta">
-          <div><a href="#">Sept 10, 2018</a></div>
-          <div><a href="#">Admin</a></div>
-          <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-      </div>
-      <h3 class="heading mt-2"><a href="#">Skin Care for Teen Skin</a></h3>
-      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-  </div>
-</div>
-</div>
-<div class="col-md-4 d-flex ftco-animate">
-    <div class="blog-entry align-self-stretch">
-      <a href="blog-single.html" class="block-20" style="background-image: url('pretty-gh-pages/images/image_3.jpg');">
-      </a>
-      <div class="text py-4 d-block">
-        <div class="meta">
-          <div><a href="#">Sept 10, 2018</a></div>
-          <div><a href="#">Admin</a></div>
-          <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-      </div>
-      <h3 class="heading mt-2"><a href="#">Skin Care for Teen Skin</a></h3>
-      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-  </div>
-</div>
-</div>
-</div>
-</div>
 </section>
 
 @endsection
